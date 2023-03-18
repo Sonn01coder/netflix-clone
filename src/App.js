78032f376055ff367180413291f4b3471702b1db
Nguyen Truong Son <92424848+Sonn01coder@users.noreply.github.com>
2023-03-19 02:54:21 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter>
           <Navbar />
           <Routes>
-            <Route exact path='/netflix-clone' element={<Home/>} />
+            <Route exact path='/' element={<Home/>} />
             <Route path='/search' element={<Search />} />
           </Routes>
           <Footer />
